601744554c4c59fc7335d88f308dd0c3ebb48fd0
sentonliam-tech <sentonliam@gmail.com>
2026-02-07 14:10:21 -0500

    turret subsystem

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -5,11 +5,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretSubsystem extends SubsystemBase {
     TalonFX turretMotor=new TalonFX(41); 
-
+double targetPositon = 0;
+PIDController turretController=new PIDController(0.001, 0, 0);
     public TurretSubsystem() {
 TalonFXConfiguration config=new TalonFXConfiguration();
 config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
@@ -21,5 +23,15 @@ public void setPower (double power) {
 turretMotor.set(power);
 
     
+}
+public double getCurrentPosition() {
+return turretMotor.getPosition().getValueAsDouble();
+
+}
+public void setTargetPositon(double targetPositon) {
+this.targetPositon = targetPositon;
+}
+public void runTurretToTarget(){
+    setPower( turretController.calculate(getCurrentPosition(),targetPositon));
 }
 }
