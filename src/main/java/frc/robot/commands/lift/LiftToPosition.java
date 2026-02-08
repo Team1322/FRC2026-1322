0485561bd140bb9fa3126e43672457f32ecea44a
Alex Clute <55024407+Robotic-Racer14a@users.noreply.github.com>
2026-02-08 17:33:22 -0500

    Removed unused import

--- src/main/java/frc/robot/commands/lift/LiftToPosition.java
+++ src/main/java/frc/robot/commands/lift/LiftToPosition.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.lift;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import static frc.robot.SystemVariables.LiftConstants;
 import frc.robot.subsystems.LiftSubsystem;
 
 public class LiftToPosition extends Command {
