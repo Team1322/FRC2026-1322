c1b0fc3df8a54296de1ade48a66b688faa625d69
Alex Clute <55024407+Robotic-Racer14a@users.noreply.github.com>
2026-02-13 15:21:03 -0500

    added javadoc to drive to pose

--- src/main/java/frc/robot/DriveToPoseObject.java
+++ src/main/java/frc/robot/DriveToPoseObject.java
@@ -6,6 +6,17 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.measure.LinearVelocity;
 import frc.robot.SystemVariables.DrivetrainConstants;
 
+/**
+ * Defines the way drive to pose works by using 3 possible parameters
+ * <p>
+ * All DriveToPoseObjects must have a Pose2d passed in, this is the target for this move
+ * <p>
+ * The class also holds an optional parameter of distance (in meters) until the move contines on and also makes it a continous move
+ * <p>
+ * Another optional parameter is the stepSpeed, which is the max speed the robot can acheive during that move
+ * <ul>
+ *   <li> Defined by doing MetersPerSecond.of(double);
+ */
 public class DriveToPoseObject {
 
     private final Pose2d pose;
--- src/main/java/frc/robot/commands/drive/DriveToPose.java
+++ src/main/java/frc/robot/commands/drive/DriveToPose.java
@@ -8,6 +8,14 @@ import frc.robot.DriveToPoseObject;
 import frc.robot.SystemVariables.DrivetrainConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
+/**
+ * Drive the robot in a straight line towards a target pose with two modes
+ * <p>
+ * Fine Move - Goes from pose to pose slowing to a stop at each pose
+ * <p>
+ * Continous Move - Drives in the direction of the next pose while the speed is driven by the end pose
+ * and will move to the next pose when it reaches the desginated distance
+ */
 public class DriveToPose extends Command {
 
     private final DriveSubsystem drive;
@@ -19,6 +27,11 @@ public class DriveToPose extends Command {
     StructPublisher<Pose2d> publisher = NetworkTableInstance.getDefault()
         .getStructTopic("Drive To Pose Target", Pose2d.struct).publish();
     
+        /**
+         * Creates an instance of the Drive To Pose Command
+         * @param drive Current instance of the drive subsystem
+         * @param intermediatePoses Drive To Pose targets that defines where and how the robot drives
+         */
     public DriveToPose (DriveSubsystem drive, DriveToPoseObject... intermediatePoses) {
         this.drive = drive;
         this.targetPoses = intermediatePoses;  
