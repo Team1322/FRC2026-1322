6b0f797ec108328d77285ef58fcea9e411af44f0
Alex Clute <55024407+Robotic-Racer14a@users.noreply.github.com>
2026-02-20 11:29:12 -0500

    clean up

--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.SystemVariables.LiftConstants;
-import frc.robot.SystemVariables.TurretConstants;
 
 public class LiftSubsystem extends SubsystemBase {
 
