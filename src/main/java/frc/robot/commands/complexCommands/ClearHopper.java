09ab1227ba4ac9ad3ed8d0e5766eb889faf4db2d
sentonliam-tech <sentonliam@gmail.com>
2026-02-28 11:34:16 -0500

    the worst part of this proses

--- src/main/java/frc/robot/commands/complexCommands/ClearHopper.java
+++ src/main/java/frc/robot/commands/complexCommands/ClearHopper.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.SystemVariables;
 import frc.robot.SystemVariables.LiftConstants.LiftStates;
 import frc.robot.commands.feeder.RunFeeder;
+import frc.robot.commands.lift.HopperNotStill;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.LiftSubsystem;
 
@@ -15,12 +16,8 @@ public class ClearHopper extends ParallelCommandGroup {
     public ClearHopper (FeederSubsystem feeder, LiftSubsystem lift) {
         addCommands(
             new ConditionalCommand(new RunFeeder(feeder), new WaitCommand(0), () -> SystemVariables.shooterUpToSpeed),
-            new SequentialCommandGroup(
-                new InstantCommand(() -> lift.setTargetState(LiftStates.DISTURB)),
-                new WaitCommand(0.3),
-                new InstantCommand(() -> lift.setTargetState(LiftStates.INTAKE)),
-                new WaitCommand(0.3)
-            )
+           new HopperNotStill(lift)
+            
             );
     }
 
