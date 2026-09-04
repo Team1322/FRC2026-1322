d2816e7f024db785bb3830317e2caf460e5376e4
alexc <alexc@vrs-inc.com>
2026-09-04 12:20:34 -0400

    Should fix issue with feeder going too early

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -54,7 +54,7 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public boolean isShooterSpunUp()  {
-        return Math.abs(shooterMotor.getVelocity().getValueAsDouble() - velo) < 20 && velo > 5;
+        return Math.abs(shooterMotor.getVelocity().getValueAsDouble() - velo) < 20 && shooterMotor.getVelocity().getValueAsDouble() > 5;
     }
 
     public void setShootFromDistance() {
